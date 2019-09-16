46ddcfdc165d15fb3ecdc7e7f4c6a7ba00c126d8
Russell Thackston <github@thackston.me>
2019-09-16 17:23:11 -0400

    Added primary key and auto increment flags

--- sql/IT5236.sql
+++ sql/IT5236.sql
@@ -1,5 +1,5 @@
 CREATE TABLE doList (
-  listID int(32) NOT NULL,
+  listID int(32) NOT NULL PRIMARY KEY AUTO_INCREMENT,
   complete tinyint(1) DEFAULT NULL,
   listItem varchar(100) NOT NULL,
   finishDate date DEFAULT NULL
